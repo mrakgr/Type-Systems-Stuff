c1651aea5a02dba8c18292001e9ab6d2801aa163
Marko Grdinić <mrakgr@gmail.com>
2017-02-14 14:18:29 +0100

    Done translating the core parts, but I am not even close to being done with the whole example.
    I have to go through the relevant book chapters again to figure out what the point of all of this machinery is.

---
+++ Untyped Lambda Calculus/FullUntyped.fs
@@ -0,0 +1,129 @@
+﻿module FullUntyped.Main
+
+(* ------------------------   EVALUATION  ------------------------ *)
+
+exception NoRuleApplies
+
+let rec isnumericval ctx = function
+    | TmZero(_) -> true
+    | TmSucc(_,t1) -> isnumericval ctx t1
+    | _ -> false
+
+/// Returns whether the val is in context.
+let rec isval ctx = function
+    | TmTrue(_)  -> true
+    | TmFalse(_) -> true
+    | TmFloat _  -> true
+    | TmString _  -> true
+    | t when isnumericval ctx t  -> true
+    | TmAbs(_,_,_) -> true
+    | TmRecord(_,fields) -> List.forall (fun (l,ti) -> isval ctx ti) fields
+    | _ -> false
+
+let rec eval1 ctx = function
+    | TmIf(_,TmTrue(_),t2,t3) -> t2
+    | TmIf(_,TmFalse(_),t2,t3) -> t3
+    | TmIf(fi,t1,t2,t3) ->
+        let t1' = eval1 ctx t1
+        TmIf(fi, t1', t2, t3)
+    | TmVar(fi,n,_) ->
+        match getbinding fi ctx n with
+        | TmAbbBind(t) -> t 
+        | _ -> raise NoRuleApplies
+    | TmApp(fi,TmAbs(_,x,t12),v2) when isval ctx v2 ->
+        termSubstTop v2 t12
+    | TmApp(fi,v1,t2) when isval ctx v1 ->
+        let t2' = eval1 ctx t2
+        TmApp(fi, v1, t2')
+    | TmApp(fi,t1,t2) ->
+        let t1' = eval1 ctx t1
+        TmApp(fi, t1', t2)
+    | TmRecord(fi,fields) ->
+        let rec evalafield = function
+            | [] -> raise NoRuleApplies
+            | (l,vi)::rest when isval ctx vi -> 
+                let rest' = evalafield rest
+                (l,vi)::rest'
+            | (l,ti)::rest -> 
+                let ti' = eval1 ctx ti
+                (l, ti')::rest
+        let fields' = evalafield fields
+        TmRecord(fi, fields')
+    | TmProj(fi, (TmRecord(_, fields) as v1), l) when isval ctx v1 ->
+        match List.tryFind (fun x -> fst x = l) fields with
+        | Some x -> snd x
+        | None -> raise NoRuleApplies
+    | TmProj(fi, t1, l) ->
+        let t1' = eval1 ctx t1
+        TmProj(fi, t1', l)
+    | TmTimesfloat(fi,TmFloat(_,f1),TmFloat(_,f2)) ->
+        TmFloat(fi, f1 * f2)
+    | TmTimesfloat(fi,(TmFloat(_,f1) as t1),t2) ->
+        let t2' = eval1 ctx t2
+        TmTimesfloat(fi,t1,t2') 
+    | TmTimesfloat(fi,t1,t2) ->
+        let t1' = eval1 ctx t1
+        TmTimesfloat(fi,t1',t2) 
+    | TmSucc(fi,t1) ->
+        let t1' = eval1 ctx t1
+        TmSucc(fi, t1')
+    | TmPred(_,TmZero(_)) ->
+        TmZero(dummyinfo)
+    | TmPred(_,TmSucc(_,nv1)) when isnumericval ctx nv1 ->
+        nv1
+    | TmPred(fi,t1) ->
+        let t1' = eval1 ctx t1 in
+        TmPred(fi, t1')
+    | TmIsZero(_,TmZero(_)) ->
+        TmTrue(dummyinfo)
+    | TmIsZero(_,TmSucc(_,nv1)) when isnumericval ctx nv1 ->
+        TmFalse(dummyinfo)
+    | TmIsZero(fi,t1) ->
+        let t1' = eval1 ctx t1 in
+        TmIsZero(fi, t1')
+    | TmLet(fi,x,v1,t2) when isval ctx v1 ->
+        termSubstTop v1 t2 
+    | TmLet(fi,x,t1,t2) ->
+        let t1' = eval1 ctx t1 in
+        TmLet(fi, x, t1', t2) 
+    | _ -> 
+        raise NoRuleApplies
+
+let rec eval ctx t =
+  try let t' = eval1 ctx t
+      in eval ctx t'
+  with NoRuleApplies -> t
+
+let evalbinding ctx = function
+    | TmAbbBind(t) ->
+        let t' = eval ctx t
+        TmAbbBind(t')
+    | bind -> bind
+
+let test = """/* Examples for testing */
+
+true;
+if false then true else false; 
+
+x/;
+x;
+
+x = true;
+x;
+if x then false else x; 
+
+lambda x. x;
+(lambda x. x) (lambda x. x x); 
+
+{x=lambda x.x, y=(lambda x.x)(lambda x.x)}; 
+{x=lambda x.x, y=(lambda x.x)(lambda x.x)}.x; 
+
+"hello";
+
+timesfloat (timesfloat 2.0 3.0) (timesfloat 4.0 5.0);
+
+0; 
+succ (pred 0);
+iszero (pred (succ (succ 0))); 
+
+let x=true in x;"""
---
+++ Untyped Lambda Calculus/FullUntypedSyntax.fs
@@ -0,0 +1,148 @@
+﻿[<AutoOpen>]
+module FullUntyped.Syntax
+
+type info = FI of string * int * int | UNKNOWN
+type 'a withinfo = {i: info; v: 'a}
+
+let dummyinfo = UNKNOWN
+let createInfo f l c = FI(f, l, c)
+
+(* ---------------------------------------------------------------------- *)
+(* Datatypes *)
+
+type term =
+| TmTrue of info
+| TmFalse of info
+| TmIf of info * term * term * term
+| TmVar of info * int * int
+| TmAbs of info * string * term
+| TmApp of info * term * term
+| TmRecord of info * (string * term) list
+| TmProj of info * term * string
+| TmFloat of info * float
+| TmTimesfloat of info * term * term
+| TmString of info * string
+| TmZero of info
+| TmSucc of info * term
+| TmPred of info * term
+| TmIsZero of info * term
+| TmLet of info * string * term * term
+
+type binding =
+| NameBind 
+| TmAbbBind of term
+
+type context = (string * binding) list
+
+type command =
+| Eval of info * term
+| Bind of info * string * binding
+
+(* ---------------------------------------------------------------------- *)
+(* Context management *)
+
+let emptycontext = []
+
+let ctxlength ctx = List.length ctx
+
+let addbinding ctx x bind = (x,bind)::ctx
+
+let addname ctx x = addbinding ctx x NameBind
+
+let rec isnamebound ctx x =
+    match ctx with
+    | [] -> false
+    | (y,_)::rest ->
+        if y=x then true
+        else isnamebound rest x
+
+let rec pickfreshname ctx x =
+  if isnamebound ctx x then pickfreshname ctx (x + "'")
+  else ((x,NameBind)::ctx), x
+
+let printInfo = function
+  | FI(f,l,c) -> sprintf "%s:%i.%i:" f l c
+  | UNKNOWN -> "<Unknown file and line>: "
+
+let error fi s = 
+    failwithf "Error:%s\n%s\n" (printInfo fi) s
+
+let index2name fi ctx x =
+  try
+    let (xn,_) = List.item x ctx
+    xn
+  with Failure _ ->
+    let msg = sprintf "Variable lookup failure: offset: %d, ctx size: %d"
+    error fi (msg x (List.length ctx))
+
+let rec name2index fi ctx x =
+  match ctx with
+      [] -> error fi ("Identifier " + x + " is unbound")
+    | (y,_)::rest ->
+        if y=x then 0
+        else 1 + (name2index fi rest x)
+
+(* ---------------------------------------------------------------------- *)
+(* Shifting *)
+
+let tmmap onvar c t = 
+    let rec walk c t = 
+        match t with
+        | TmTrue(fi) as t -> t
+        | TmFalse(fi) as t -> t
+        | TmIf(fi,t1,t2,t3) -> TmIf(fi,walk c t1,walk c t2,walk c t3)
+        | TmVar(fi,x,n) -> onvar fi c x n
+        | TmAbs(fi,x,t2) -> TmAbs(fi,x,walk (c+1) t2)
+        | TmApp(fi,t1,t2) -> TmApp(fi,walk c t1,walk c t2)
+        | TmProj(fi,t1,l) -> TmProj(fi,walk c t1,l)
+        | TmRecord(fi,fields) ->
+            TmRecord(fi,List.map (fun (li,ti) -> (li,walk c ti)) fields)
+        | TmFloat _ as t -> t
+        | TmTimesfloat(fi,t1,t2) -> TmTimesfloat(fi, walk c t1, walk c t2)
+        | TmString _ as t -> t
+        | TmZero(fi)      -> TmZero(fi)
+        | TmSucc(fi,t1)   -> TmSucc(fi, walk c t1)
+        | TmPred(fi,t1)   -> TmPred(fi, walk c t1)
+        | TmIsZero(fi,t1) -> TmIsZero(fi, walk c t1)
+        | TmLet(fi,x,t1,t2) -> TmLet(fi,x,walk c t1,walk (c+1) t2)
+    walk c t
+
+let termShiftAbove d c t =
+    tmmap (fun fi c x n -> if x>=c then TmVar(fi,x+d,n+d) else TmVar(fi,x,n+d)) c t
+
+let termShift d t = termShiftAbove d 0 t
+
+let bindingshift d bind =
+    match bind with
+    | NameBind -> NameBind
+    | TmAbbBind(t) -> TmAbbBind(termShift d t)
+
+(* ---------------------------------------------------------------------- *)
+(* Substitution *)
+
+let termSubst j s t =
+    tmmap (fun fi c x n -> if x=j+c then termShift c s else TmVar(fi,x,n)) 0 t
+
+let termSubstTop s t = 
+    termShift (-1) (termSubst 0 (termShift 1 s) t)
+
+(* ---------------------------------------------------------------------- *)
+(* Context management (continued) *)
+
+let rec getbinding fi ctx i =
+  try
+    let (_,bind) = List.item i ctx
+    bindingshift (i+1) bind 
+  with Failure _ ->
+    let msg = Printf.sprintf "Variable lookup failure: offset: %d, ctx size: %d"
+    error fi (msg i (List.length ctx))
+ 
+(* ---------------------------------------------------------------------- *)
+(* Extracting file info *)
+
+let tmInfo t = 
+    match t with
+    | TmTrue(fi) | TmFalse(fi) | TmIf(fi,_,_,_) | TmVar(fi,_,_)
+    | TmAbs(fi,_,_) | TmApp(fi, _, _) | TmProj(fi,_,_) | TmRecord(fi,_)
+    | TmFloat(fi,_) | TmTimesfloat(fi,_,_) | TmString(fi,_) | TmZero(fi)
+    | TmSucc(fi,_) | TmPred(fi,_) | TmIsZero(fi,_) | TmLet(fi,_,_,_) -> fi 
