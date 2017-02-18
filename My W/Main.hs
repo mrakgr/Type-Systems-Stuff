717d526abcf48bc9a140d97a689499adbadf088b
Marko Grdinić <mrakgr@gmail.com>
2017-02-18 10:38:02 +0100

    I finished translating Oleg's example to F# and testing it to boot. Before finishing the job, I want to convert the example to a monadic Haskell one with the state and the exception monads.
    The way exceptions are used for errors in the F# example is kind of a pain in the ass.
    
    I wrote that monads were a pain yesterday, but I doubt they would be worse than converting this whole thing to use its own combinators.
    
    Let me do it and then I will take a look at the other Algorithm W examples.

--- My W/Main.hs
+++ My W/Main.hs
@@ -12,8 +12,6 @@ import Data.IntMap(IntMap)
 import qualified Data.Map as M
 import Data.Map(Map)
 
-
-
 type VarName = String
 data Term
   = V VarName
@@ -31,122 +29,130 @@ type TVarName = Int
 infixr 9 :>
 
 data TVE = TVE Int (IntMap Typ)
+type TEnv = Map VarName Typ
+data MainEnv = MainEnv {tve :: TVE, tenv :: TEnv}
+
+get_tenv = liftM tenv get
+get_tve = liftM tve get
+modify_tenv f = modify (\x -> x { tenv = f $ tenv x})
+modify_tve f = modify (\x -> x { tve = f $ tve x})
 
 --newtv :: TVE -> (Typ, TVE)
 newtv = do
-  (TVE n s) <- get
-  put $ TVE (n+1) s
-  return $ TV n
+  TVE n s <- get_tve
+  modify_tve (const $ TVE (n+1) s)
+  return (TV n)
 
 tve0 :: TVE
 tve0 = TVE 0 IM.empty
 
-tvlkup :: TVE -> TVarName -> Maybe Typ
-tvlkup (TVE _ s) v = IM.lookup v s
+--tvlkup :: TVE -> TVarName -> Maybe Typ
+tvlkup v = do
+  TVE _ s <- get_tve
+  return $ IM.lookup v s
 
-tvext :: TVE -> (TVarName, Typ) -> TVE
-tvext (TVE c s) (tv,t) = TVE c (IM.insert tv t s)
+--tvext :: TVE -> (TVarName, Typ) -> TVE
+tvext (tv,t) = modify_tve $ \(TVE c s) -> TVE c (IM.insert tv t s)
 
-tvsub :: TVE -> Typ -> Typ
-tvsub tve (t1 :> t2) = tvsub tve t1 :> tvsub tve t2
-tvsub tve (TV v) | Just t <- tvlkup tve v = tvsub tve t
-tvsub tve t = t
+--tvsub :: TVE -> Typ -> Typ
+tvsub (t1 :> t2) = liftM2 (:>) (tvsub t1) (tvsub t2)
+tvsub t@(TV v) =
+  tvlkup v >>= \case
+    Just t' -> tvsub t'
+    Nothing -> return t
+tvsub t = return t
 
-unify = \t1 t2 -> do
-  tve <- get
-  unify' (tvchase tve t1) (tvchase tve t2) tve where
+-- chase through a substitution ‘shallowly’:
+-- stop at the last equivalent type variable
+--tvchase :: TVE -> Typ -> Typ
+tvchase t@(TV v) =
+  tvlkup v >>= \case
+    Just t' -> tvsub t'
+    Nothing -> return t
+tvchase t = return t
+--
+-- If either t1 or t2 are type variables, they must be unbound
+unify' TInt TInt = return ()
+unify' (t1a :> t1r) (t2a :> t2r) = do
+  () <- unify t1r t2r
+  () <- unify t1a t2a
+  return ()
+unify' (TV v1) t2 = unifyv v1 t2
+unify' t1 (TV v2) = unifyv v2 t1
+unify' t1 t2 = throwError $ Left $ unwords ["constant mismatch:", show t1, show t2]
 
-  -- chase through a substitution ‘shallowly’:
-  -- stop at the last equivalent type variable
-  tvchase :: TVE -> Typ -> Typ
-  tvchase tve (TV v) | Just t <- tvlkup tve v = tvchase tve t
-  tvchase _ t = t
+unifyv v1 (TV v2) | v1 == v2 = return ()
+unifyv v1 t2 =
+  occurs v1 t2 >>= \case
+    True -> do
+      m <- tvsub t2
+      throwError $ Left $ unwords ["occurs check:", show (TV v1), "in", show m]
+    False -> tvext (v1,t2) -- record new constraint
 
-  -- If either t1 or t2 are type variables, they must be unbound
-  unify' TInt TInt = return ()
-  unify' (t1a :> t1r) (t2a :> t2r) = do
-    _ <- unify t1r t2r
-    _ <- unify t1a t2a
-    return ()
-  unify' (TV v1) t2 = unifyv v1 t2
-  unify' t1 (TV v2) = unifyv v2 t1
-  unify' t1 t2 = throwError $ Left $ unwords ["constant mismatch:", show t1, show t2]
+occurs v TInt = return False
+occurs v (t1 :> t2) = liftM2 (||) (occurs v t1) (occurs v t2)
+occurs v (TV v2) =
+  tvlkup v2 >>= \case
+    Nothing -> return (v == v2)
+    Just t -> occurs v t
 
-  unifyv v1 (TV v2) | v1 == v2 = return ()
+unify t1 t2 = do
+  t1' <- tvchase t1
+  t2' <- tvchase t2
+  unify' t1' t2'
 
-  unifyv v1 t2 = do
-    is_occurs <- occurs v1 t2
-
-    if is_occurs then
-      throwError $ Left $ unwords ["occurs check:", show (TV v1), "in", show (tvsub tve t2)]
-    else do
-      modify $ \tve -> tvext tve (v1,t2)
-      return ()  -- record new constraint
-
-    where
-      -- I've rewriten this like so because the lookup might otherwise miss the case where
-      -- v1 and TV v2 are equal if it is called from outside of unifyv.
-      -- While Oleg's version works as intended, having the occurs function at the top level
-      -- definitely implies things about its structure that are false.
-      occurs v TInt = return False
-      occurs v (t1 :> t2) = liftM2 (||) (occurs v t1) (occurs v t2)
-      occurs v (TV v2) = do
-        tve <- get
-        case tvlkup tve v2 of
-          Nothing -> return False -- This is always false because of the `unifyv v1 (TV v2) tve | v1 == v2 = Right tve` line
-          Just t -> occurs v t tve
-
-type TEnv = Map VarName Typ
-
-lkup env x = fromMaybe err $ M.lookup x env
-  where err = error $ "Unbound variable " ++ x
+lkup x = do
+  tenv <- get_tenv
+  M.lookup x tenv >>= \case
+    Just x -> return x
+    Nothing -> return $ throwError $ Left $ "Unbound variable " ++ x
 env0 = M.empty
-ext = M.insert
+ext k v = modify_tenv (M.insert k v)
+--
+-- -- teval' :: TEnv -> Term -> TVE -> (Typ,TVE)
+-- teval' = \case
+--   V x -> lkup x
+--   L x e -> do
+--     tv <- newtv
+--     te <- teval' (ext x tv tenv) e
+--     return (tv :> te)
+--     A e1 e2 -> do
+--       t1 <- teval' tenv e1
+--       t2 <- teval' tenv e2
+--       t1r <- newtv
+--       unify t1 (t2 :> t1r) >>= \case
+--         Right tve -> return (t1r,tve)
+--         Left err -> error err
+--     I n ->
+--       return TInt
+--     e1 :+ e2 ->
+--       let (t1,tve) = teval' tenv e1 tve in
+--       let (t2,tve) = teval' tenv e2 tve in
+--       case either Left (unify t2 TInt) $  unify t1 TInt tve of
+--         Right tve -> (TInt,tve)
+--         Left err -> error $ "Trying to add non-integers: " ++ err
+--     Fix e ->
+--       let (t,tve) = teval' tenv e tve in
+--       let (ta,tve) = newtv tve in
+--       let (tb,tve) = newtv tve in
+--       -- It never occurred to me, but it maybe the `->` type arrows are right associative in ML?
+--       case unify t ((ta :> tb) :> ta :> tb) tve of
+--         Right tve -> (ta :> tb,tve)
+--         Left err -> error $ "Inappropriate type in Fix: " ++ err
+--
+-- teval :: TEnv -> Term -> Typ
+-- teval tenv e =
+--   let (t,tve) = teval' tenv e tve0 in
+--   tvsub tve t
+--
+-- (vx,vy) = (V "x", V "y")
+-- test0 = teval env0 ((L "x" (vx :+ (I 2))) `A` (I 1))
+-- -- (TV 1,TVE 2 (fromList [(0,TInt),(1,TInt)]))
+-- term1 = L "x" (IFZ vx (I 1) (vx :+ (I 2)))
+-- test10 = teval env0 term1
+-- -- (TV 0 :> TInt,TVE 1 (fromList [(0,TInt)]))
+--
+-- test1 = teval env0 term1 -- TInt :> TInt
 
---teval' :: TEnv -> Term -> TVE -> (Typ,TVE)
-teval' tenv t =
-  case t of
-    V x -> return (lkup tenv x)
-    L x e -> do
-      tv <- newtv
-      te <- teval' (ext x tv tenv) e
-      return (tv :> te)
-    A e1 e2 -> do
-      t1 <- teval' tenv e1
-      t2 <- teval' tenv e2
-      t1r <- newtv
-      unify t1 (t2 :> t1r) >>= \case
-        Right tve -> return (t1r,tve)
-        Left err -> error err
-    I n ->
-      return TInt
-    e1 :+ e2 ->
-      let (t1,tve) = teval' tenv e1 tve in
-      let (t2,tve) = teval' tenv e2 tve in
-      case either Left (unify t2 TInt) $  unify t1 TInt tve of
-        Right tve -> (TInt,tve)
-        Left err -> error $ "Trying to add non-integers: " ++ err
-    Fix e ->
-      let (t,tve) = teval' tenv e tve in
-      let (ta,tve) = newtv tve in
-      let (tb,tve) = newtv tve in
-      -- It never occurred to me, but it maybe the `->` type arrows are right associative in ML?
-      case unify t ((ta :> tb) :> ta :> tb) tve of
-        Right tve -> (ta :> tb,tve)
-        Left err -> error $ "Inappropriate type in Fix: " ++ err
-
-teval :: TEnv -> Term -> Typ
-teval tenv e =
-  let (t,tve) = teval' tenv e tve0 in
-  tvsub tve t
-
-(vx,vy) = (V "x", V "y")
-test0 = teval env0 ((L "x" (vx :+ (I 2))) `A` (I 1))
--- (TV 1,TVE 2 (fromList [(0,TInt),(1,TInt)]))
-term1 = L "x" (IFZ vx (I 1) (vx :+ (I 2)))
-test10 = teval env0 term1
--- (TV 0 :> TInt,TVE 1 (fromList [(0,TInt)]))
-
-test1 = teval env0 term1 -- TInt :> TInt
-
-main = print $ test0
+-- main = print $ test0
+main = return ()
--- Oleg's Algorithm W/Program.fs
+++ Oleg's Algorithm W/Program.fs
@@ -1,4 +1,7 @@
-﻿type VarName = string
+﻿// From http://okmij.org/ftp/Computation/FLOLAC/lecture.pdf
+// Interpreting Types as Abstract Values
+
+type VarName = string
 type Term =
   | V of VarName
   | L of VarName * Term
@@ -56,23 +59,19 @@ let rec unify t1 t2 tve =
         | t1, t2 -> Fail <| sprintf "constant mismatch: %A %A" t1 t2
 
     and unifyv v1 t2 tve =
-//        I've rewriten occurs like so because the lookup might otherwise miss the case where
-//        v1 and TV v2 are equal if it is called from outside of unifyv.
-//        While Oleg's version works as intended, having the occurs function at the top level
-//        definitely implies things about its structure that are false.
-        let rec occurs v t tve =
-            match t with
-            | TInt -> false
-            | TArr(t1,t2) -> occurs v t1 tve || occurs v t2 tve
-            | TV v2 ->
-                match tvlkup tve v2 with
-                | None -> false // This is always false because of the `| TV v2 when v1 = v2 -> Succ tve` line
-                | Some t -> occurs v t tve
-
         match t2 with
         | TV v2 when v1 = v2 -> Succ tve
         | _ when occurs v1 t2 tve -> Fail <| sprintf "occurs check: %A in %A" (TV v1) (tvsub tve t2)
         | _ -> Succ <| tvext tve (v1,t2) // record new constraint
+
+    and occurs v t tve =
+        match t with
+        | TInt -> false
+        | TArr(t1,t2) -> occurs v t1 tve || occurs v t2 tve
+        | TV v2 ->
+            match tvlkup tve v2 with
+            | None -> v = v2 
+            | Some t -> occurs v t tve
 
     unify' (tvchase tve t1) (tvchase tve t2) tve
 
@@ -83,51 +82,114 @@ let lkup env x =
     | Some v -> v
     | None -> failwithf "Unbound variable %A" x
 
-let env0() = Map.empty
+let env0 = Map.empty
 let ext k v m = Map.add k v m
 
-//teval' :: TEnv -> Term -> TVE -> (Typ,TVE)
 let rec teval' tenv t tve =
-  match t with
-  | V x -> (lkup tenv x, tve)
-  | L (x, e) ->
-      let tx,tve = newtv tve
-      let te,tve = teval' (ext x tx tenv) e tve
-      (TArr(tx,te), tve)
-  | A (e1, e2) ->
-      let t1,tve = teval' tenv e1 tve
-      let t2,tve = teval' tenv e2 tve
-      let t1r,tve = newtv tve
-      match unify t1 (TArr(t2, t1r)) tve with
-      | Succ tve -> (t1r,tve)
-      | Fail err -> failwith err
-  | I n -> (TInt, tve)
-  | Plus(e1, e2) ->
-      let t1,tve = teval' tenv e1 tve
-      let t2,tve = teval' tenv e2 tve
-      match unify t1 TInt tve |?> unify t2 TInt with
-      | Succ tve -> (TInt,tve)
-      | Fail err -> failwithf "Trying to add non-integers: %s" err
-  | Fix e ->
-      let t,tve = teval' tenv e tve
-      let ta,tve = newtv tve
-      let tb,tve = newtv tve
-      // It never occurred to me, but it maybe the `->` type arrows are right associative in ML?
-      match unify t (TArr(TArr(ta, tb), TArr(ta,tb))) tve with
-      | Succ tve -> (TArr(ta, tb),tve)
-      | Fail err -> failwithf "Inappropriate type in Fix: %s" err
+    match t with
+    | V x -> (lkup tenv x, tve)
+    | L (x, e) ->
+        let tx,tve = newtv tve
+        let te,tve = teval' (ext x tx tenv) e tve
+        (TArr(tx,te), tve)
+    | A (e1, e2) ->
+        let t1,tve = teval' tenv e1 tve
+        let t2,tve = teval' tenv e2 tve
+        let t1r,tve = newtv tve
+        match unify t1 (TArr(t2, t1r)) tve with
+        | Succ tve -> (t1r,tve)
+        | Fail err -> failwith err
+    | I n -> (TInt, tve)
+    | Plus(e1, e2) ->
+        let t1,tve = teval' tenv e1 tve
+        let t2,tve = teval' tenv e2 tve
+        match unify t1 TInt tve |?> unify t2 TInt with
+        | Succ tve -> (TInt,tve)
+        | Fail err -> failwithf "Trying to add non-integers: %s" err
+    | Fix e ->
+        let t,tve = teval' tenv e tve
+        let ta,tve = newtv tve
+        let tb,tve = newtv tve
+        // It never occurred to me, but it maybe the `->` type arrows are right associative in ML?
+        match unify t (TArr(TArr(ta, tb), TArr(ta,tb))) tve with
+        | Succ tve -> (TArr(ta, tb),tve)
+        | Fail err -> failwithf "Inappropriate type in Fix: %s" err
     | IFZ(c,tr,fa) ->
         let t_c, tve = teval' tenv c tve
         let t_tr, tve = teval' tenv tr tve
         let t_fa, tve = teval' tenv fa tve
-        let _, tve = 
-            match unify t_c TInt tve with
-            | Succ tve -> TInt, tve
-            | Fail err -> failwithf "Wrong type (%A) in the conditional of if: %s" t_c err
-        match unify t_tr t_fa tve with
-        | Succ tve -> t_tr,tve
-        | Fail err -> failwith "The two branches of if do not match. The types are true(%A), false(%A): %s" t_tr t_fa err
+        match unify t_c TInt tve with
+        | Succ tve -> 
+            match unify t_tr t_fa tve with
+            | Succ tve -> t_tr,tve
+            | Fail err -> failwithf "The two branches of if do not match. %s" err
+        | Fail err -> failwithf "Wrong type in the conditional of ifz: %s" err
+
 
 let teval tenv e =
-  let t,tve = teval' tenv e tve0
-  tvsub tve t
+    let t,tve = teval' tenv e tve0
+    tvsub tve t
+
+let (|+) a b = Plus(a,b)
+let (|%) a b = A(a,b)
+let L x f = L(x,f)
+let IFZ c t f = IFZ(c,t,f)
+
+let (vx,vy) = (V "x", V "y")
+
+let test0 = teval' env0 ((L "x" (vx |+ (I 2))) |% (I 1)) tve0
+// (TV 1,TVE 2 (fromList [(0,TInt),(1,TInt)]))
+let term1 = L "x" (IFZ vx (I 1) (vx |+ (I 2)))
+let test10 = teval' env0 term1 tve0
+// (TV 0 :> TInt,TVE 1 (fromList [(0,TInt)]))
+
+let test1 = teval env0 term1 // TInt :> TInt
+
+let tmul = 
+    Fix (L "self" 
+            (L "x" 
+                (L "y"
+                    (IFZ 
+                        vx 
+                        (I 0)
+                        (((V "self") |% (vx |+ (I (-1))) |% vy) |+ vy)))))
+
+let testm21 = teval env0 tmul // TInt :> TInt :> TInt
+let testm22 = teval env0 (tmul |% (I 2)) // TInt :> TInt
+let testm23 = teval env0 (tmul |% (I 2) |% (I 3)) // TInt
+let testm24 = teval env0 (tmul |% (I (-1)) |% (I (-1))) // TInt
+
+//let term4 = L "x" (IFZ vx (I 1) (vx |% (I 1)))
+//let test4 = teval env0 term4
+// *** Exception: Trying to compare a non-integer to 0:
+// constant mismatch: TInt :> TV 1 and TInt
+
+//let delta = L "y" (vy |% vy)
+//let testd = teval env0 delta
+// *** Exception: occurs check: TV 0 in TV 0 :> TV 1
+
+let term2a = L "x" (L "y" (vx |% vy))
+let test2a = teval env0 term2a
+// (TV 1 :> TV 2) :> (TV 1 :> TV 2)
+
+let termid = L "x" vx
+let testid = teval env0 termid 
+// TV 0 :> TV 0
+
+let term2id = 
+    L "f" 
+        (L "y" 
+            ((I 2) |+ ((termid |% (V "f")) |% ((termid |% vy) |+ (I 1)))))
+let test2id = teval env0 term2id // (TInt :> TInt) :> (TInt :> TInt)
+
+let termlet = 
+    let c2 = L "f" (L "x" (V "f" |% (V "f" |% vx)))
+    let inc = L "x" (vx |+ (I 1))
+    let compose = 
+        L "f" 
+            (L "g" 
+                (L "x"
+                    (V "f" |% (V "g" |% vx))))
+    c2 |% (compose |% inc |% inc) |% (I 10) |+ ((c2 |% (compose |% inc) |% termid) |% (I 100))
+
+let testlet = teval env0 termlet
